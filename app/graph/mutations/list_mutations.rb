b759e1e825091c91d252d4b95d8d54233fb76370
Tessa Thornton <tessathornton@gmail.com>
2017-02-06 13:18:03 -0500

    update lists

--- app/graph/mutations/list_mutations.rb
+++ app/graph/mutations/list_mutations.rb
@@ -1,6 +1,7 @@
 class ListMutations
 
   Create = GraphQL::Relay::Mutation.define do
+    name "CreateList"
     input_field :name, !types.String
 
     return_field :list, ListType
@@ -8,6 +9,25 @@ class ListMutations
     resolve -> (object, inputs, ctx) {
       user = ctx[:current_user]
       list = user.lists.create!(name: inputs[:name])
+
+      {
+        list: list
+      }
+    }
+  end
+
+  Update = GraphQL::Relay::Mutation.define do
+    name "UpdateList"
+    input_field :id, !types.ID
+    input_field :name, !types.String
+
+    return_field :list, ListType
+
+    resolve -> (object, inputs, ctx) {
+      user = ctx[:current_user]
+      type, id = GraphQL::Schema::UniqueWithinType.decode(inputs[:id])
+      list = user.lists.find(id)
+      list.update_attributes(name: inputs[:name])
 
       {
         list: list
--- app/graph/types/mutation_type.rb
+++ app/graph/types/mutation_type.rb
@@ -2,4 +2,5 @@ MutationType = GraphQL::ObjectType.define do
   name 'Mutation'
 
   field :createList, field: ListMutations::Create.field
+  field :updateList, field: ListMutations::Update.field
 end
