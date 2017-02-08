031ee00762aa6f50d0fbb7ec4431412db99aa718
Tessa Thornton <tessathornton@gmail.com>
2017-02-07 20:16:18 -0500

    list mutation

--- app/assets/javascripts/components/lists.js
+++ app/assets/javascripts/components/lists.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
+import React, {PropTypes} from 'react';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import List from './list';
+
+class NewList extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault()
+    this.props.onSubmit(this.refs.name.value);
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <label><input ref="name" placeholder="name" /></label>
+        <button type="submit">create</button>
+      </form>
+    )
+  }
+}
+
+NewList.propTypes = {
+  onSubmit: PropTypes.func.isRequired
+}
+
+const submitList = gql`
+  mutation createList($list: CreateListInput!) {
+    createList(input: $list) {
+      list {
+        name,
+        id
+      }
+    }
+  }
+`
+
 
 const listQuery = gql`query Lists { lists { id, name }  }`
 
 class Lists extends React.Component {
+  createNewList(name) {
+    this.props.mutate({
+      variables: {
+        list: {
+          name
+        }
+      }
+    }).then((data) => {
+        console.log(data);
+    });
+  }
   render() {
     return (
       <div>
         <h1>Lists</h1>
+        <NewList onSubmit={this.createNewList.bind(this)}/>
         { this.props.data.loading
           ? 'loading'
           : this.props.data.lists.map((list, key) => {
@@ -21,6 +64,16 @@ class Lists extends React.Component {
   }
 }
 
+Lists.propTypes = {
+  data: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    lists: PropTypes.array
+  }),
+  mutate: PropTypes.func.isRequired
+}
 
-export default graphql(listQuery)(Lists);
+export default compose(
+  graphql(listQuery),
+  graphql(submitList)
+)(Lists)
 
