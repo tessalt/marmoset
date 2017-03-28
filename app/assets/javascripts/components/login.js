25f5d44567e710437aa12e68f19154c876d81863
Tessa Thornton <tessathornton@gmail.com>
2017-03-27 20:48:32 -0400

    style forms

--- app/assets/javascripts/components/login.js
+++ app/assets/javascripts/components/login.js
@@ -35,13 +35,24 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label><input ref="email" placeholder="email" defaultValue="joe@example.com" /></label>
-        <label><input ref="pass" placeholder="password" /></label> (hint: password1)<br />
-        <button type="submit">login</button>
-        {this.state.error && (
-          <p>Bad login information</p>
-        )}
+      <form className="mw6 pa4 center bg-light-purple mt4 white" onSubmit={this.handleSubmit.bind(this)}>
+        <h1 className="mt0">Login</h1>
+        <div className="mt2">
+          <label className="db fw4 lh-copy f6">Email address </label>
+          <input className="pa2 input-reset ba bg-white w-100" ref="email" type="email" />
+        </div>
+        <div className="mt2">
+          <label className="db fw4 lh-copy f6">Password</label>
+          <input className="pa2 input-reset ba bg-white w-100" ref="pass" type="password" />
+        </div>
+        <div className="mt2">
+          <button className="f6 link dim bn ph3 pv2 bg-black-70 white" type="submit">Login</button>
+        </div>
+        <div className="mt2">
+          {this.state.error && (
+            <p>Bad login information</p>
+          )}
+        </div>
       </form>
     )
   }
--- app/assets/javascripts/components/signup.js
+++ app/assets/javascripts/components/signup.js
@@ -38,14 +38,28 @@ export default class Signup extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit.bind(this)}>
-        <label>email: <input ref="email" placeholder="email" defaultValue="joe@example.com" /></label><br/>
-        <label>password: <input ref="pass" placeholder="password" /></label><br />
-        <label>password confirmation: <input ref="pass_confirmation" placeholder="password" /></label><br />
-        <button type="submit">login</button>
-        {this.state.error && (
-          <p>Bad login information</p>
-        )}
+      <form className="mw6 pa4 center mt4 bg-light-purple white" onSubmit={this.handleSubmit.bind(this)}>
+        <h1 className="mt0">Signup</h1>
+        <div className="mt2">
+          <label className="db fw4 lh-copy f6">Email address </label>
+          <input className="pa2 input-reset ba bg-white w-100" ref="email" type="email" />
+        </div>
+        <div className="mt2">
+          <label className="db fw4 lh-copy f6">Password</label>
+          <input className="pa2 input-reset ba bg-white w-100" ref="pass" type="password" />
+        </div>
+        <div className="mt2">
+          <label className="db fw4 lh-copy f6">Confirm password</label>
+          <input className="pa2 input-reset ba bg-white w-100" ref="pass_confirmation" type="password"  />
+        </div>
+        <div className="mt2">
+          <button className="f6 link dim bn ph3 pv2 bg-black-70 white" type="submit">Signup</button>
+        </div>
+        <div className="mt2">
+          {this.state.error && (
+            <p>Bad login information</p>
+          )}
+        </div>
       </form>
     )
   }
